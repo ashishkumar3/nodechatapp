97eaef0aedc52fd1b0bd40eb232dc4e8276d876d
ashishkumar3 <ashishklingwal@gmail.com>
2018-10-13 00:49:44 +0530

    added message event

--- server/server.js
+++ server/server.js
@@ -1,10 +1,34 @@
 const path = require("path");
+const http = require("http");
 const express = require("express");
-const app = express();
+const app = express(http);
+const socketIO = require("socket.io");
 
 const publicPath = path.join(__dirname, "../public");
 const PORT = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const server = http.createServer(app);
+const io = socketIO(server); // io => mian connection - server to client
+
+// sockets are users / clients
+
+io.on("connection", socket => {
+  console.log("A new user connected");
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected");
+  });
+
+  socket.on("createMessage", function(message) {
+    // io.emit -> to everysingle connection
+    io.emit("newMessage", {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+});
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
